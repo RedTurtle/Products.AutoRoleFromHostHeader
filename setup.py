07f6ad19d52f19fe3402dafd17019f0f7a61cfb8
Luca Fabbri <luca@keul.it>
2013-08-05 12:04:16 +0200

    fixes after moving to github

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.3.0'
+version = '0.3.1'
 
 setup(name='Products.AutoRoleFromHostHeader',
       version=version,
       description="Plone PAS plugin for adding roles (or groups) to anonymous or logged-in "
                   "visitors looking for HTTP Headers.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
