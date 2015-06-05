e279c01c060cced2c0630a88a37da846f56376b1
Luca Fabbri <luca@keul.it>
2015-06-05 10:30:13 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.0'
+version = '1.0.1.dev0'
 setup(name='Products.AutoRoleFromHostHeader',
       version=version,
       description="Plone PAS plugin for adding roles (or groups) to anonymous or logged-in "
