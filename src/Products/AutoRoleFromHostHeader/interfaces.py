42a3a29584686d7ef7210fd4d371dd01e76e9bea
Manuel Reinhardt <reinhardt@syslab.com>
2019-06-21 14:43:03 +0200

    Python 3: Replaced class advice (implements) with decorator (implementer).

--- src/Products/AutoRoleFromHostHeader/interfaces.py
+++ src/Products/AutoRoleFromHostHeader/interfaces.py
@@ -1,9 +1,10 @@
 from zope.component.interfaces import IObjectEvent, ObjectEvent
-from zope.interface import implements
+from zope.interface import implementer
 
 
 class IConfigurationChangedEvent(IObjectEvent):
     """Fired when the ip_roles property has changed."""
 
+@implementer(IConfigurationChangedEvent)
 class ConfigurationChangedEvent(ObjectEvent):
-    implements(IConfigurationChangedEvent)
+    pass
