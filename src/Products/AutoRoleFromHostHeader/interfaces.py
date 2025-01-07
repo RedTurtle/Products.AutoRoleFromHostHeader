da8cd740e3fe031881db73e235f06b773f298553
ale-rt <alessandro.pisa@gmail.com>
2025-01-07 16:41:46 +0100

    Compatibility with Plone 6

--- src/Products/AutoRoleFromHostHeader/interfaces.py
+++ src/Products/AutoRoleFromHostHeader/interfaces.py
@@ -1,5 +1,12 @@
-from zope.component.interfaces import IObjectEvent, ObjectEvent
 from zope.interface import implementer
+
+try:
+    from zope.interface.interfaces import IObjectEvent
+    from zope.interface.interfaces import ObjectEvent
+except ImportError:
+    # BBB
+    from zope.component.interfaces import IObjectEvent
+    from zope.component.interfaces import ObjectEvent
 
 
 class IConfigurationChangedEvent(IObjectEvent):
